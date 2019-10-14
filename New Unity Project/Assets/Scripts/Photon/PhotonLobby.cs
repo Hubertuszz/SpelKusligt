872c3114af86d3d283ec0a41d2de45bc80e905bc
Hugo Mattsson <hugo.mattsson@elev.ga.klaragymnasium.se>
2019-10-14 15:46:23 +0200

    Start Up

---
+++ New Unity Project/Assets/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,75 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+    public GameObject searchButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this; //Creates the singleton
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Master photon server.
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        PhotonNetwork.AutomaticallySyncScene = true;
+        searchButton.SetActive(true);
+        
+    }
+
+    public void OnSearchButtonClicked()
+    {
+        Debug.Log("Search Button was clicked");
+        searchButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. There must be no open games available");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Trying to create room");
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 5 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed, there must already be a room with the same name");
+        CreateRoom();
+    }
+
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("Cancel button clicked");
+        cancelButton.SetActive(false);
+        searchButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
