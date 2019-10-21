f613bfee8227b9b547adfe0b695d34706c92df77
Hugo Mattsson <hugo.mattsson@elev.ga.klaragymnasium.se>
2019-10-21 09:55:39 +0200

    Lagt in en till grabb för test och fixat lite shit

---
+++ SpelKusligt/Assets/Scripts/Photon/GameControllers/AvatarSetup.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarSetup : MonoBehaviour
+{
+    private PhotonView pv;
+    public int characterValue;
+    public GameObject myCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+        if (pv.IsMine)
+        {
+            pv.RPC("RPC_AddCharacter", RpcTarget.AllBuffered, PlayerInfo.pi.selectedCharacter);
+        }
+    }
+
+    [PunRPC]
+    void RPC_AddCharacter(int randomCharacter)
+    {
+        myCharacter = Instantiate(PlayerInfo.pi.allCharacters[randomCharacter], transform.position, transform.rotation, transform);
+    }
+    
+}
