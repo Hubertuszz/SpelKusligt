2fb9de5e14c81195310c394f7e7da67fd84ff9c2
SchlyfeMormor <44261512+SchlyfeMormor@users.noreply.github.com>
2019-10-21 10:34:21 +0200

    movement i guess

--- SpelKusligt/Assets/Scripts/Photon/GameControllers/PlayerMovement.cs
+++ SpelKusligt/Assets/Scripts/Photon/GameControllers/PlayerMovement.cs
@@ -7,15 +7,16 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private PhotonView pv;
-    private CharacterController myCC;
-    public float movementSpeed;
-    public float rotationSpeed;
+    public float speed = 4.0f;
+    public float gravity = -9.8f;
+
+    private CharacterController _charCont;
 
     // Start is called before the first frame update
     void Start()
     {
         pv = GetComponent<PhotonView>();
-        myCC = GetComponent<CharacterController>();
+        _charCont = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -24,12 +25,40 @@ public class PlayerMovement : MonoBehaviour
         if (pv.IsMine)
         {
             BasicMovement();
-            BasicRotation();
+            //BasicRotation();
         }
     }
 
     void BasicMovement()
     {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed); //Limits the max speed of the player
+
+        // movement.y = gravity;
+
+        movement *= Time.deltaTime;     //Ensures the speed the player moves does not change based on frame rate
+        movement = transform.TransformDirection(movement);
+        _charCont.Move(movement);
+    }
+
+   
+}
+
+ /*
+    void BasicRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+        transform.Rotate(new Vector3(0, mouseX, 0));
+    }
+    */
+/* void start
+        pv = GetComponent<PhotonView>();
+        myCC = GetComponent<CharacterController>();
+        */
+
+/* basicmovement
         if (Input.GetKey(KeyCode.W))
         {
             myCC.Move(transform.forward * Time.deltaTime * movementSpeed);
@@ -46,10 +75,4 @@ public class PlayerMovement : MonoBehaviour
         {
             myCC.Move(transform.right * Time.deltaTime * movementSpeed);
         }
-    }
-    void BasicRotation()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
-        transform.Rotate(new Vector3(0, mouseX, 0));
-    }
-}
+        */
